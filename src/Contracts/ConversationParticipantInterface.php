595965077a68b0ea3a1b8f3a23e438d62cb71d3c
Ange Arsene <nkenmandenga@gmail.com>
2026-01-18 21:39:23 +0100

    fix: Add getKey method declaration to ConversationParticipantInterface to properly documents the contract that all participants must provide.

--- src/Contracts/ConversationParticipantInterface.php
+++ src/Contracts/ConversationParticipantInterface.php
@@ -6,5 +6,10 @@ namespace AngeArsene\Chat\Contracts;
 
 interface ConversationParticipantInterface
 {
-    
+    /**
+     * Get the unique identifier for the participant.
+     *
+     * @return mixed
+     */
+    public function getKey();
 }
